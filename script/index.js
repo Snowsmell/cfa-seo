3dce8a27dd2103afadfe59f761c265a28c818725
薛启伟 <snowsmell@aliyun.com>
2018-02-23 11:21:26 +0800

    sidebar和移动端倒计时clear

--- script/index.js
+++ script/index.js
@@ -1,7 +1,39 @@
 $(function(){
   function countdown(target){
     var days=Math.floor((new Date(target)-new Date)/1000/60/60/24);
-    $('.countdown b').html(days)
+    $('aside .countdown b').html(days)
+    var arr =days.toString().split('');
+    var text='';
+    arr.forEach(function(v,i){
+      if(v!=0 || i!=0){
+        text +="<b>"+v+"</b>"
+      }
+    })
+    $('.mbc>div')[0].innerHTML = text+' 天'
   }
   countdown('2018/6/23')
+
+
+  //banner划出后显示导航
+  function navshow(){
+    $(window).on('scroll',function(){
+      if($(window).width() > 1024 && $(window).scrollTop()>$('.banner').height()){
+        $('nav.sidebar').fadeIn()
+      }else{
+        $('nav.sidebar').fadeOut()
+      }
+    })
+  }
+  navshow()
+
+  //考试成绩切换
+  function changerate(){
+    $('.passrate .divide a').on('click',function(){
+      $(this).addClass('current').siblings().removeClass('current')
+      $('.passrate .imgbox img').eq($(this).index())
+                                .addClass('current')
+                                .siblings().removeClass('current')
+    })                          
+  }
+  changerate()
 })
